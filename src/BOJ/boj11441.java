ebabdf56cdd200d2f003ec31035eeb383fa911e0
huh0529 <huh0529@naver.com>
2021-07-14 15:26:55 +0900

    실3 11441 합 구하기 - 누적합

---
+++ src/BOJ/boj11441.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class boj11441 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A= new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=1;i<=N;i++){
+            A[i]=Integer.parseInt(st.nextToken());
+            A[i]+=A[i-1];
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        for(int m=0;m<M;m++){
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            System.out.println(A[j]-A[i-1]);
+        }
+    }
+}
