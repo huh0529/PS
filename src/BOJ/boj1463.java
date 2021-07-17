a0e24e353101605b0d927f4628c02eaa63b477ca
huh0529 <huh0529@naver.com>
2021-07-17 15:40:32 +0900

    실3 1463 1로 만들기 - dp

---
+++ src/BOJ/boj1463.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj1463 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N + 1];
+
+
+        for (int i = 2; i <= N; i++) {
+            if (i % 2 == 0)
+                if (i % 3 == 0)
+                    A[i] = Math.min(A[i - 1], Math.min(A[i / 2], A[i / 3])) + 1;
+                else
+                    A[i] = Math.min(A[i - 1], A[i / 2]) + 1;
+
+            else if (i % 3 == 0)
+                A[i] = Math.min(A[i - 1], A[i / 3]) + 1;
+
+            else
+                A[i] = A[i - 1] + 1;
+        }
+
+        System.out.println(A[N]);
+    }
+}
