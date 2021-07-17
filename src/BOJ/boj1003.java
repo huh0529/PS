bc1aeeba6a38b00e0238c28c3c0346c8f67ff7b3
huh0529 <huh0529@naver.com>
2021-07-18 03:16:45 +0900

    실3 1003 피보나치 함수 - dp

---
+++ src/BOJ/boj1003.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj1003 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int N;
+        int[][] dp=new int[41][2];
+
+        dp[0][0]=1;
+        dp[0][1]=0;
+
+        dp[1][0]=0;
+        dp[1][1]=1;
+
+        for(int i=2;i<=40;i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+
+        for(int t=0;t<T;t++){
+            N = Integer.parseInt(br.readLine());
+
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+    }
+}
