9b29da35d4aec352df25bae93068f473cfcdaa75
huh0529 <huh0529@naver.com>
2021-07-18 15:58:42 +0900

    실1 11048 이동하기 - dp

---
+++ src/BOJ/boj11048.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11048 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] dp=new int[N+1][M+1];
+
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=M;j++){
+                dp[i][j]=Integer.parseInt(st.nextToken()) + Math.max(dp[i][j-1],Math.max(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        System.out.println(dp[N][M]);
+    }
+}
