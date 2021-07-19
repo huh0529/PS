4f83187e9c3917ac782c782eb1d421e04c606452
huh0529 <huh0529@naver.com>
2021-07-19 14:57:59 +0900

    실1 2468 안전 영역 - dfs

---
+++ src/BOJ/boj2468.java
@@ -0,0 +1,63 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class boj2468 {
+    static int[][] A;
+    static int[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        A=new int[N+2][N+2];
+
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+
+            for(int j=1;j<=N;j++){
+                A[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max=0;
+
+        for(int h=0;h<100;h++) {
+            int cnt = 0;
+            visited = new int[N + 2][N + 2];
+
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (A[i][j] > h && visited[i][j] == 0) {
+                        cnt++;
+                        dfs(i, j, h);
+                    }
+                }
+            }
+            max = Math.max(max, cnt);
+        }
+
+        System.out.println(max);
+    }
+
+    static void dfs(int x, int y, int h) {
+        if (visited[x][y] == 1)
+            return;
+
+        if (A[x][y] <= h)
+            return;
+
+        visited[x][y] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            dfs(x + dx[i], y + dy[i], h);
+        }
+    }
+}
