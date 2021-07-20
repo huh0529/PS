e9a034bb1c9b9ed31c0949edd8f5fb280f94557d
huh0529 <huh0529@naver.com>
2021-07-21 05:34:18 +0900

    실3 15652 N과 M(4) - 백트래킹

---
+++ src/BOJ/boj15652.java
@@ -0,0 +1,41 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj15652 {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        func(0);
+        System.out.println(sb);
+
+    }
+
+    static void func(int m) {
+        if (m == M) {
+            for (int i = 0; i < M; i++)
+                sb.append(arr[i]).append(" ");
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (m > 0 && i < arr[m - 1])
+                continue;
+            arr[m] = i;
+            func(m + 1);
+        }
+    }
+}
