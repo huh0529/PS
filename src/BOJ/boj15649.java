1ce9fe48eb50bfafa1f0f526a20fb7721766bc76
huh0529 <huh0529@naver.com>
2021-07-21 05:26:35 +0900

    실3 15649 N과 M(1) - 백트래킹

---
+++ src/BOJ/boj15649.java
@@ -0,0 +1,44 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj15649 {
+    static int N, M;
+    static int[] arr;
+    static boolean[] isUsed;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        isUsed = new boolean[N+1];
+        func(0);
+
+    }
+
+    static void func(int m) {
+        if (m == M) {
+            for (int i = 0; i < M; i++)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                arr[m] = i;
+                func(m + 1);
+                isUsed[i] = false;
+            }
+        }
+
+    }
+}
