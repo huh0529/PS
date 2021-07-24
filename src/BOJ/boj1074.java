c0bb47c32a2e449705f0bea9626bf92c16b22e86
huh0529 <huh0529@naver.com>
2021-07-24 16:34:15 +0900

    실1 1074 Z - 재귀 / 분할 정복

---
+++ src/BOJ/boj1074.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1074 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(func(N, r, c));
+    }
+
+    static int func(int N, int r, int c) {
+        int mid = (int) Math.pow(2, N - 1);
+
+        if(N==0)
+            return 0;
+
+        if (r < mid && c < mid)
+            return func(N - 1, r, c);
+        if (r < mid && c >= mid)
+            return mid*mid + func(N-1, r, c-mid);
+        if (r >= mid && c < mid)
+            return 2*mid*mid +func(N-1, r-mid, c);
+        if (r >= mid && c >= mid)
+            return 3*mid*mid +func(N-1, r-mid, c-mid);
+
+        return 0;
+    }
+}
