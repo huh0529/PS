4ad3173b5802cf9c7169592532a66513a364fc11
huh0529 <huh0529@naver.com>
2021-08-05 10:21:28 +0900

    실3 9461 파도반 수열 - dp

---
+++ src/BOJ/dp/boj9461.java
@@ -0,0 +1,29 @@
+package BOJ.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj9461 {
+    public static void main(String[] args) throws Exception {
+
+        long[] dp = new long[101];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+
+            System.out.println(dp[N]);
+        }
+    }
+}
