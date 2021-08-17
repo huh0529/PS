4569e2b0c90a632fd9c45879fc400d6244746f94
huh0529 <huh0529@naver.com>
2021-08-18 01:27:50 +0900

    .

--- src/prgms/표편집.java
+++ src/prgms/표편집.java
@@ -9,9 +9,8 @@ class Solution {
         int[] arr = new int[1000];
         Stack<Integer> Zstack = new Stack<>(); //int형 스택 선언
         ArrayList<Integer> list = new ArrayList<>();
-
         int last = n - 1;
-        int x=0;
+        int x = 0;
 
         for (int i = 0; i < n; i++) {
             list.add(i);
@@ -21,41 +20,50 @@ class Solution {
             switch (cmd[i].charAt(0)) {
                 case 'U':
                     x = Integer.parseInt(cmd[i].replaceAll("[^0-9]", ""));
-                    while (x>0) {
-                        k--;
-                        if (arr[k] == 1)
-                            continue;
-                        x--;
-                    }
+                    k -= x;
                     break;
                 case 'D':
                     x = Integer.parseInt(cmd[i].replaceAll("[^0-9]", ""));
-                    while (x>0) {
-                        k++;
-                        if (arr[k] == 1)
-                            continue;
-                        x--;
-                    }
+                    k += x;
                     break;
                 case 'C':
-                    arr[k] = 1;
-
-                    Zstack.push(k);
+                    arr[list.get(k)] = 1;
+                    Zstack.push(list.get(k));
+                    list.remove(k);
 
                     if (k == last) {
                         k--;
-                        while (arr[k] == 1)
-                            k--;
                         last = k;
-                    }
-                    else {
-                        k++;
-                        while (arr[k] == 1)
-                            k++;
                     }
                     break;
                 case 'Z':
-                    arr[Zstack.pop()] = 0;
+                    int z = Zstack.pop();
+                    arr[z] = 0;
+
+                    if(z<list.get(k))
+                        k++;
+
+                    if(list.get(last)<z){
+                        list.add(z);
+                    }
+                    else{
+                        //list에 이분탐색으로 위치 집어넣기
+                        int low = 0;
+                        int high = last;
+                        int mid;
+
+                        while(low<high) {
+                            mid = (low + high) / 2;
+
+                            if(list.get(mid)>=z)
+                                high=mid;
+                            else
+                                low=mid+1;
+
+                        }
+                        list.add(high, z);
+                    }
+
                     break;
             }
         }
@@ -67,6 +75,7 @@ class Solution {
                 answer += 'X';
         }
 
+        System.out.println(answer);
         return answer;
     }
 }
