e9952e01786a84fcfd4483f46cac194208308e2f
huh0529 <huh0529@naver.com>
2021-11-28 23:54:12 +0900

    level1 완주하지 못한 선수 - 해시

---
+++ src/prgms/해시/완주하지못한선수.java
@@ -0,0 +1,20 @@
+package prgms.해시;
+
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+
+        for(int i = 1; i < phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])) {
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
