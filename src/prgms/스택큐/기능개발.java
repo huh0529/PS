8e2e088085d99aa2296d553e9b7d3aec6a95996f
huh0529 <huh0529@naver.com>
2021-11-28 23:52:58 +0900

    level2 기능개발 - 스택큐

---
+++ src/prgms/스택큐/기능개발.java
@@ -0,0 +1,35 @@
+package prgms.스택큐;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+
+        List<Integer> answer = new ArrayList<>();
+
+        int t = 0;
+
+        while (t < progresses.length) {
+            int cnt=0;
+
+            for (int i = t; i < progresses.length; i++) {
+                progresses[i] += speeds[i];
+            }
+
+            for (int i = t; i < progresses.length; i++) {
+                if (progresses[i] >= 100) {
+                    t++;
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            if(cnt>0) {
+                answer.add(cnt);
+            }
+        }
+
+        return answer;
+    }
+}
