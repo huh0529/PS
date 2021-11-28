abae4a867734e15e5e144a21cef4d3d68c4d059d
huh0529 <huh0529@naver.com>
2021-11-28 23:53:32 +0900

    level2 프린터 - 스택큐

---
+++ src/prgms/스택큐/프린터.java
@@ -0,0 +1,33 @@
+package prgms.스택큐;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
+        }
+
+        int t = 0;
+
+        while (priorities[location] != 0) {
+            while (priorities[t] == 0) {
+                t = (t + 1) % priorities.length;
+            }
+
+            if (priorities[t] == pq.peek()) {
+                pq.remove();
+                priorities[t] = 0;
+                answer++;
+            } else {
+                t = (t + 1) % priorities.length;
+            }
+        }
+        return answer;
+    }
+}
