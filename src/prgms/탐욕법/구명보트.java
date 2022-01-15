6b7aa70fb3d9a19221ecc576545c4a69a91fd4b2
huh0529 <huh0529@naver.com>
2022-01-16 06:33:08 +0900

    L2 구명보트 - 그리디, 투포인터

---
+++ src/prgms/탐욕법/구명보트.java
@@ -0,0 +1,24 @@
+package prgms.탐욕법;
+
+import java.util.Arrays;
+
+class Solution4 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
