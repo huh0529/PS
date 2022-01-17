4d60585d1b3e26db980c144313b8acced79f01d6
huh0529 <huh0529@naver.com>
2022-01-17 21:18:52 +0900

    D3 10726 이진수표현 - 비트연산자

---
+++ src/SWEA/swea10726.java
@@ -0,0 +1,23 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class swea10726 {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            //이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            if ((M & (1<<N)-1) == (1<<N)-1){
+                System.out.println("#" + test_case + " ON");
+            } else
+                System.out.println("#" + test_case + " OFF");
+        }
+    }
+}
