68123e8b28ff60d643557071a04869883bc23c5f
huh0529 <huh0529@naver.com>
2022-01-26 13:42:58 +0900

    D? 3998 Inversion Counting - 분할정복(합병정렬)
    합병정렬을 하면서 a[i]>a[j] 일때 정렬하면서 cnt에 m-i+1 만큼 추가

---
+++ src/SWEA/분할정복/swea3998.java
@@ -0,0 +1,26 @@
+package SWEA.분할정복;
+
+import java.util.Scanner;
+
+public class swea3998 {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            //이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            
+
+
+        }
+    }
+}
