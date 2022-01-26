4a0b57ca75f36a6c843d6c5a50b68feeb92f78cc
huh0529 <huh0529@naver.com>
2022-01-26 15:06:59 +0900

    D? 3998 Inversion Counting - 분할정복(합병정렬)	(수정)
    합병정렬을 하면서 a[i]>a[j] 일때 정렬하면서 cnt에 m-i+1 만큼 추가

--- src/SWEA/분할정복/swea3998.java
+++ src/SWEA/분할정복/swea3998.java
@@ -3,6 +3,11 @@ package SWEA.분할정복;
 import java.util.Scanner;
 
 public class swea3998 {
+    static int N;
+    static int[] arr;
+    static int[] tmp;
+    static long cnt;
+
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T;
@@ -11,16 +16,50 @@ public class swea3998 {
         for (int test_case = 1; test_case <= T; test_case++) {
             //이 부분에 여러분의 알고리즘 구현이 들어갑니다.
 
-            int N = sc.nextInt();
-            int[] arr = new int[N];
-
+            N = sc.nextInt();
+            arr = new int[N];
+            tmp = new int[N];
+            cnt = 0;
             for (int i = 0; i < N; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            
+            mergeSort(0, N - 1);
 
+            System.out.println("#" + test_case + " " + cnt);
+        }
+    }
 
+    static void mergeSort(int left, int right) {
+        if (left == right) {
+            return;
+        }
+
+        int mid = (left + right) / 2;
+
+        mergeSort(left, mid);
+        mergeSort(mid + 1, right);
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] > arr[j]) {
+                tmp[k++] = arr[j++];
+                cnt += mid - i + 1;
+            }else
+                tmp[k++] = arr[i++];
+        }
+        while (i <= mid) {
+            tmp[k++] = arr[i++];
+        }
+        while (j <= right) {
+            tmp[k++] = arr[j++];
+        }
+
+        for (int l = left; l <= right; l++) {
+            arr[l] = tmp[l];
         }
     }
 }
